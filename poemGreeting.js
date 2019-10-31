760b9b615b0ddb6680afa93cb7273bbb85b04620
gabisch18 <50716756+gabisch18@users.noreply.github.com>
2019-10-31 15:46:56 -0400

    Poem Greetings for New Followers

---
+++ poemGreeting.js
@@ -0,0 +1,11 @@
+module.exports = function (name) {
+  return (
+// insert your greeting message
+`
+A poem for ${name}:
+Figure Out How to make a poem here.
+Also found a site for adding emojis
+`
+//
+  )
+}
