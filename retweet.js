b35875532fb68e2e52508f4178ec1108386029f7
gabisch18 <50716756+gabisch18@users.noreply.github.com>
2019-11-07 11:46:35 -0500

    Update retweet.js

--- retweet.js
+++ retweet.js
@@ -1,31 +1,42 @@
+//Finds tweets with hashtag Poetry, Poem, Poems, and PoetryLover.
+
 var Twit = require('twit');
 
 var T = new Twit(require('./config.js'));
 
-var poetrySearch = {q: "#poetry", count: 10, result_type: "recent"}; 
+//Search for posts on Twitter with the hashtag Poetry, Poem, Poems, and PoetryLover. Also filter out any retweeted posts.
+var poetrySearch = {q: "#poetry OR #poem OR #poems OR #poetrylover, -filter:retweets", count: 10, result_type: "recent"}; 
 
+//Retweet the last (nonretweeted) posts found by poetrySearch
 function retweetLatest() {
+    console.log("calling get() with poetrySearch()");
 	T.get('search/tweets', poetrySearch, function (error, data) {
         
 	  console.log(error, data);
+        //check for any occurring errors
 	  if (!error) {
+          //Obtain id of tweet being retweeted
 		var retweetId = data.statuses[0].id_str;
+          //Post the tweet obtained
 		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+            //Retweeting Successful
 			if (response) {
 				console.log('Success! Check your bot, it should have retweeted something.')
 			}
-
+            //Retweeting Failure - Twitter Call
 			if (error) {
 				console.log('There was an error with Twitter:', error);
 			}
 		})
 	  }
+        //Retweeting Failure - Search Request
 	  else {
 	  	console.log('There was an error with your hashtag search:', error);
 	  }
 	});
 }
-
+//Calling retweet function - launch point of events
 retweetLatest();
-//every half hour?
+//retweet every half hour
 setInterval(retweetLatest, 1000 * 60 * 30);
+
